a5edb9b5fbf7cab2231c27c588acd93476459b2e
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-18 14:58:47 +0700

    Gud

--- src/component/avatar.js
+++ src/component/avatar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function avatar() {
   return (
     <div>
-      <img src="profile1.png" width="300"></img>
+      <img src="profile1.png" width="200"></img>
     </div>
   );
 }
