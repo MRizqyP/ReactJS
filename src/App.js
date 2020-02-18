b296f6181d35aafee51152cd995225ab47e04ecb
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-18 10:52:14 +0700

    sett

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { Container, Row, Col } from "reactstrap";
+
+import Hello from "./component/hello";
+import Clock from "./component/clock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Hello name="Rizqy" />
+      <Clock />
+    </Container>
   );
 }
 
