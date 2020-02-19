5f9ace1993c4f4d96863a301dcee323cb6b60e89
Mohammad Rizqy Pratama <stukang123@gmail.com>
2020-02-19 09:47:07 +0700

    done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import { Container, Row, Col } from "reactstrap";
+import Navbar from "./component/navbar";
 
 import Userprofile from "./component/userprofile";
 function App() {
   return (
     <center>
+      <Navbar />
       <Userprofile />
+
       {/* //{" "}
       <Container>
         // <Row>// </Row>
